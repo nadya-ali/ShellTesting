04ead3f7fe08a2c49b7c613810bc0987da0e3fac
aeschenko <nonpafos@gmail.com>
2013-01-17 16:13:04 +0400

    fixed tabs/spaces in MR_SmokeTest, added release 2.05 apk file to test

--- MR_SmokeTest.py
+++ MR_SmokeTest.py
@@ -17,45 +17,45 @@ mfuncs.SetGlobVars(int(majorAndr), folder_name, model, isCrashExit)
 (appPath, appName, build, branch, cmp) = adbcom_module.GetapkFile()
 
 def run () :
-	from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
-	currentCarouselPanels = defaultCarouselPanels = 7
-	defaultFloorPanels = 6
-	WidgetSet = (16, 9, 2, 2)
-	if model=='GT-I9100' or model=='m0' or model=='GT-N7000' or model=='t03g' or 2.3 > float(andr):
-		WidgetSet = (16, 8, 2, 2)
-	print 'starting test'
-	mfuncs.mAct('BACK', 2)
-	time.sleep(1)
-	mfuncs.addYandexWidgets(WidgetSet)
-	mfuncs.mAct('BACK', 2)
-	mfuncs.DialerContactsFavourites()
-	mfuncs.DialerContacts()
-	mfuncs.MessagesCheck()
-	mfuncs.BrowserCheck()
-	adbcom_module.ShellStart(appPath, cmp, 2)
-	mfuncs.SearchCheck()
+    from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+    currentCarouselPanels = defaultCarouselPanels = 7
+    defaultFloorPanels = 6
+    WidgetSet = (16, 9, 2, 2)
+    if model=='GT-I9100' or model=='m0' or model=='GT-N7000' or model=='t03g' or 2.3 > float(andr):
+        WidgetSet = (16, 8, 2, 2)
+    print 'starting test'
+    mfuncs.mAct('BACK', 2)
+    time.sleep(1)
+    mfuncs.addYandexWidgets(WidgetSet)
+    mfuncs.mAct('BACK', 2)
+    mfuncs.DialerContactsFavourites()
+    mfuncs.DialerContacts()
+    mfuncs.MessagesCheck()
+    mfuncs.BrowserCheck()
+    adbcom_module.ShellStart(appPath, cmp, 2)
+    mfuncs.SearchCheck()
 
-	#mfuncs.takePanelsToCarousel2(11, False)
-	#mfuncs.addYandexWidgets()
+    #mfuncs.takePanelsToCarousel2(11, False)
+    #mfuncs.addYandexWidgets()
 
-	mfuncs.screenshotPanels(5)
-	#widgetSettings
-	#widgetWork
+    mfuncs.screenshotPanels(5)
+    #widgetSettings
+    #widgetWork
 
-	currentCarouselPanels += defaultFloorPanels
-	currentCarouselPanels += 4
+    currentCarouselPanels += defaultFloorPanels
+    currentCarouselPanels += 4
 
-	"""
-	mfuncs.addAndroidWidgets(5)
+    """
+    mfuncs.addAndroidWidgets(5)
 
-	mfuncs.screenshotCarouselPanels(currentCarouselPanels)
+    mfuncs.screenshotCarouselPanels(currentCarouselPanels)
 
-	"""
-	print ('Made ' +mfuncs.GetScreenshoted() +' screenshots')
+    """
+    print ('Made ' +mfuncs.GetScreenshoted() +' screenshots')
 
 
 try:
-	run()
+    run()
 
 except:
-	adbcom_module.trace2log(folder_name)
+    adbcom_module.trace2log(folder_name)
